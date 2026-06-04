e559c4988005dd9944215b63206741817a60b2f7
saumya-510 <saumya_n@me.iitr.ac.in>
2026-06-04 12:57:14 +0530

    changes in frontend

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-
-const genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+require('dotenv').config();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 const parsePrescription = async (rawText) => {
   try {
