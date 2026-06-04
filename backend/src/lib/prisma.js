a6e7dc53e3064031ad4d9e78309a969203f651d4
saumya-510 <saumya_n@me.iitr.ac.in>
2026-06-04 20:48:06 +0530

    some changes after deployment

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,7 +1,13 @@
 const { PrismaClient } = require('@prisma/client')
 const { PrismaPg } = require('@prisma/adapter-pg')
+const { Pool } = require('pg')
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+})
+
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 module.exports = prisma
